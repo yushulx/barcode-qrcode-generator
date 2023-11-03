540a8fc8273751d4d341d2e3b7451a9a48d4c4f8
yushulx <lingxiao1002@gmail.com>
2023-11-03 14:41:55 +0800

    Added barcode types

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ function drawRoundedRect(ctx, x, y, width, height, radius) {
     ctx.stroke();
 }
 
+function randomPharmacodeNumber() {
+    return Math.floor(Math.random() * (131070 - 3 + 1)) + 3;
+}
+
 function generate(action) {
     const rows = document.getElementById('rows').value;
     const cols = document.getElementById('cols').value;
@@ -25,8 +29,34 @@ function generate(action) {
     const barcodeType = document.getElementById('barcodeType').value;
     const canvas = document.getElementById('barcodeCanvas');
 
-    const rectWidth = barcodeType === 'qrcode' ? 200 : 220;
-    const rectHeight = barcodeType === 'qrcode' ? 200 : 150;
+    let rectWidth = 200;
+    let rectHeight = 200;
+    if (barcodeType === 'ean13') {
+        rectWidth = 250;
+        rectHeight = 150;
+    } else if (barcodeType === 'qrcode') {
+        rectWidth = 200;
+        rectHeight = 200;
+    } else if (barcodeType === 'code128') {
+        rectWidth = 250;
+        rectHeight = 150;
+    } else if (barcodeType === 'code39') {
+        rectWidth = 480;
+        rectHeight = 150;
+    } else if (barcodeType === 'itf') {
+        rectWidth = 270;
+        rectHeight = 150;
+    } else if (barcodeType === 'msi') {
+        rectWidth = 330;
+        rectHeight = 150;
+    } else if (barcodeType === 'pharmacode') {
+        rectWidth = 160;
+        rectHeight = 150;
+    } else if (barcodeType === 'codabar') {
+        rectWidth = 320;
+        rectHeight = 150;
+    }
+
     const radius = 10;
     const spacing = 10;
     canvas.width = cols * (rectWidth + spacing);
@@ -52,7 +82,12 @@ function generate(action) {
             let value = '';
 
             if (isRandom) {
-                value = randomEAN13Number();
+                if (barcodeType === 'pharmacode') {
+                    value = randomPharmacodeNumber();
+                }
+                else {
+                    value = randomEAN13Number();
+                }
             }
             else {
                 value = text;
@@ -76,7 +111,7 @@ function generate(action) {
                         format: "ean13",
                         width: 2,
                         height: 100,
-                        displayValue: false
+                        displayValue: true
                     });
                 } else if (barcodeType === 'qrcode') {
                     const qr = qrcode(4, 'L');
@@ -91,6 +126,48 @@ function generate(action) {
                     };
 
                     continue;
+                } else if (barcodeType === 'code128') {
+                    JsBarcode(tempCanvas, value, {
+                        format: "CODE128",
+                        width: 2,
+                        height: 100,
+                        displayValue: true
+                    });
+                } else if (barcodeType === 'code39') {
+                    JsBarcode(tempCanvas, value, {
+                        format: "CODE39",
+                        width: 2,
+                        height: 100,
+                        displayValue: true
+                    });
+                } else if (barcodeType === 'itf') {
+                    JsBarcode(tempCanvas, value, {
+                        format: "ITF",
+                        width: 2,
+                        height: 100,
+                        displayValue: true
+                    });
+                } else if (barcodeType === 'msi') {
+                    JsBarcode(tempCanvas, value, {
+                        format: "MSI",
+                        width: 2,
+                        height: 100,
+                        displayValue: true
+                    });
+                } else if (barcodeType === 'pharmacode') {
+                    JsBarcode(tempCanvas, value, {
+                        format: "pharmacode",
+                        width: 2,
+                        height: 100,
+                        displayValue: true
+                    });
+                } else if (barcodeType === 'codabar') {
+                    JsBarcode(tempCanvas, value, {
+                        format: "codabar",
+                        width: 2,
+                        height: 100,
+                        displayValue: true
+                    });
                 }
 
                 // Draw rounded rect
@@ -101,7 +178,7 @@ function generate(action) {
                 ctx.drawImage(tempCanvas, x + 5, y + 5);
 
                 // Draw text below barcode
-                ctx.fillText(value, x + 45, y + 140);
+                // ctx.fillText(value, x + 45, y + 140);
             } catch (error) {
                 alert(error);
                 needStop = true;
